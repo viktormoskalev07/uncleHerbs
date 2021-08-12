4b7c922dee927c23d9b00d9c461523682ef44a4c
MatviienkoNatali <matviienko.natali@gmail.com>
2021-08-12 17:24:41 +0300

    page blog

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,17 @@ window.onload= function(){
 
 //preloader
 if (document.querySelector(".preloader")) {
+  
+  function hidescroll() {
+    body.style.paddingRight = window.innerWidth - html.offsetWidth + "px";
+    body.style.overflow = "hidden";
+  }
+  
+  function showscroll() {
+    body.style.paddingRight = 0;
+    body.style.overflow = "visible";
+  }
+
   const loader = document.querySelector(".preloader");
   loader.style.top = document.querySelector(".header").clientHeight + "px";
   hidescroll();
@@ -37,3 +48,64 @@ if (document.querySelector(".preloader")) {
 }
 //preloader
 
+//swiper
+(function () {
+  let swiper = Swiper;
+  let init = false;
+
+  function swiperMode() {
+    let mobile = window.matchMedia("(min-width: 0px) and (max-width: 991px)");
+    let desktop = window.matchMedia(
+      "(min-width: 991px) and (max-width: 40000px)"
+    );
+
+    if (mobile.matches) {
+      if (!init) {
+        init = true;
+        swiper = new Swiper(".swiper-categories", {
+          slidesPerView: 1,
+          autoplay: {
+            delay: 5000,
+          },
+          loop: true,
+          spaceBetween: 10,
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          breakpoints: {
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 5,
+            },
+            415: {
+              slidesPerView: 1.2,
+              spaceBetween: 5,
+            },
+            700: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            992: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          },
+        });
+      }
+    } else if (desktop.matches) {
+      init && swiper.destroy();
+      init = false;
+    }
+  }
+
+  if (document.querySelector(".swiper-categories")) {
+    window.addEventListener("load", function () {
+      swiperMode();
+    });
+
+    window.addEventListener("resize", function () {
+      swiperMode();
+    });
+  }
+})();
